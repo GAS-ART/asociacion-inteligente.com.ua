b13a95818303dc6761fe15ebcd3bad1b06cf64eb
Artem <temoha1386@gmail.com>
2022-08-03 16:09:01 +0300

    changed footer

--- resources/lang/ru/footer.php
+++ resources/lang/ru/footer.php
@@ -14,9 +14,9 @@
    'contacts' => 
    [
       'address' => '<p>Адрес</p><p>С/Puigcugul 48 Cassa de la Selva 17244 Girona</p>',
-      'phone' => '<p>Телефон</p><a href="tel:%2B34698560864">698560864</a>',
-      'phone_1' => '<a href="tel:%2B34664895661">664895661</a>',
-      'phone_2' => '<a href="tel:%2B34717194632">717194632</a>',
+      'phone' => '<p>Телефон</p><a href="tel:%2B34698560864">+34698560864</a>',
+      'phone_1' => '<a href="tel:%2B34664895661">+34664895661</a>',
+      'phone_2' => '<a href="tel:%2B34717194632">+34717194632</a>',
       'email' => '<p>Электронная почта</p><a href="mailto:inteli.gente.ua@gmail.com">inteli.gente.ua@gmail.com</a>',
    ],
    'donate' =>
