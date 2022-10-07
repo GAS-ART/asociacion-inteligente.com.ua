896eadee3af8586f396b5acdc1bf571c218f99e3
GAS-ART <temoha1386@gmail.com>
2022-10-07 22:22:02 +0300

    Fixed help form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,7 +7,7 @@ use Mail;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\ContactRequest;
 use App\Http\Requests\QuestionRequest;
-use App\Http\Requests\helpRequest;
+use App\Http\Requests\HelpRequest;
 
 class ContactController extends Controller
 {
